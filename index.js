4dfa4f37d91567e22034562ba36186f15fe0f4aa
Henri Korhonen <henri.korhonen@reaktor.com>
2018-04-03 07:39:25 +0300

    Remove semicolons

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-"use strict";
+const Promise = require("bluebird")
+const Docker = require("dockerode")
 
-const Promise = require("bluebird");
-const Docker = require("dockerode");
+const minimist = require("minimist")
+const request = Promise.promisifyAll(require("request"), {multiArgs: true})
 
-const minimist = require("minimist");
-const request = Promise.promisifyAll(require("request"), {multiArgs: true});
+const neighbours = require("./lib/neighbours")
+const gridLib = require("./lib/game-grid")
 
-const neighbours = require("./lib/neighbours");
-const gridLib = require("./lib/game-grid");
-
-const docker = new Docker();
-Promise.promisifyAll(docker);
+const docker = new Docker()
+Promise.promisifyAll(docker)
 
 const args = minimist(process.argv.slice(2), {
   default: {
@@ -22,24 +20,24 @@ const args = minimist(process.argv.slice(2), {
     "d": "delete"
   },
   boolean: "d"
-});
+})
 
-const generationCount = args._[0] ? args._[0] : 3;
-const fileName = args.file;
-const deleteContainersAtEnd = args.delete !== false;
-const gameGrid = gridLib.create(fileName).then(printGrid);
-const game = setupGenerations(gameGrid, generationCount);
+const generationCount = args._[0] ? args._[0] : 3
+const fileName = args.file
+const deleteContainersAtEnd = args.delete !== false
+const gameGrid = gridLib.create(fileName).then(printGrid)
+const game = setupGenerations(gameGrid, generationCount)
 
 if (deleteContainersAtEnd) {
-  addContainerDeletion(game);
+  addContainerDeletion(game)
 }
 
 function setupGenerations(gameGrid, amount) {
-  var grid = gameGrid;
+  var grid = gameGrid
   for (var i = 1; i < amount; i++) {
-    grid = grid.then(updateGrid);
-  };
-  return grid;
+    grid = grid.then(updateGrid)
+  }
+  return grid
 }
 
 function updateGrid(gridPromise) {
@@ -47,54 +45,54 @@ function updateGrid(gridPromise) {
 }
 
 function nextGenerationGrid(grid) {
-  const currentGenerationGrid = getGenerationGridValues(grid);
+  const currentGenerationGrid = getGenerationGridValues(grid)
   return Promise.all(grid.map((row) => {
     return Promise.all(row.map((container) => {
       return Promise.join(containerInfo(container.id), currentGenerationGrid, updateContainer)
     }))
-  })).thenReturn(grid);
+  })).thenReturn(grid)
 }
 
 function getGenerationGridValues(grid) {
   return Promise.all(grid.map((row) => {
     return Promise.all(row.map((container) => {
       return containerInfo(container.id).then((data) => {
-        return data.State.Running;
-      });
-    }));
-  }));
+        return data.State.Running
+      })
+    }))
+  }))
 }
 
 function containerInfo(containerId) {
-  const container = docker.getContainer(containerId);
-  Promise.promisifyAll(container);
+  const container = docker.getContainer(containerId)
+  Promise.promisifyAll(container)
   return container.inspectAsync()
 }
 
 function updateContainer(info, grid) {
-  const isRunning = info.State.Running;
-  const envVariables = info.Config.Env;
-  const x = parseInt(envVariables[0].substr(2));
-  const y = parseInt(envVariables[1].substr(2));
-  const neighbourCount = neighbours.calculate(x, y, grid);
-  const cellLives = neighbourCount == 2 || neighbourCount == 3;
-  const cellRestarts = neighbourCount == 3;
+  const isRunning = info.State.Running
+  const envVariables = info.Config.Env
+  const x = parseInt(envVariables[0].substr(2))
+  const y = parseInt(envVariables[1].substr(2))
+  const neighbourCount = neighbours.calculate(x, y, grid)
+  const cellLives = neighbourCount == 2 || neighbourCount == 3
+  const cellRestarts = neighbourCount == 3
 
   if (isRunning & !cellLives) {
-    return stopContainerWithId(info.Id);
+    return stopContainerWithId(info.Id)
   } else if (!isRunning && cellRestarts) {
-    return startContainerWithId(info.Id);
+    return startContainerWithId(info.Id)
   }
 }
 
 function stopContainerWithId(id) {
-  const container = docker.getContainer(id);
-  const promise = Promise.resolve(container);
-  return gridLib.stopContainer(promise);
+  const container = docker.getContainer(id)
+  const promise = Promise.resolve(container)
+  return gridLib.stopContainer(promise)
 }
 
 function sendUpdateRequest(info, containerGrid) {
-  const hostPort = info.NetworkSettings.Ports["8000/tcp"][0].HostPort;
+  const hostPort = info.NetworkSettings.Ports["8000/tcp"][0].HostPort
   const postOptions = {
     uri: `http://localhost:${hostPort}/update`,
     body: {
@@ -111,9 +109,9 @@ function sendUpdateRequest(info, containerGrid) {
 }
 
 function startContainerWithId(id) {
-  const container = docker.getContainer(id);
-  const promise = Promise.resolve(container);
-  return gridLib.startContainer(promise);
+  const container = docker.getContainer(id)
+  const promise = Promise.resolve(container)
+  return gridLib.startContainer(promise)
 }
 
 function printGrid(grid) {
@@ -123,11 +121,11 @@ function printGrid(grid) {
         return row.map((value) => {
           return value == true ? 'X' : '.'
         })
-      }));
-      console.log('\n');
+      }))
+      console.log('\n')
       return printGrid
     })
-    .thenReturn(grid);
+    .thenReturn(grid)
 }
 
 function addContainerDeletion(gameGrid) {
@@ -135,13 +133,13 @@ function addContainerDeletion(gameGrid) {
     return Promise.all(gameGrid.map((row) => {
       return Promise.all(row.map((container) => {
         return Promise.join(docker.getContainer(container.id), containerInfo(container.id), (container, data) => {
-          var promise = Promise.resolve(container);
+          var promise = Promise.resolve(container)
           if (data.State.Running) {
-            promise = gridLib.stopContainer(promise).thenReturn(promise);
+            promise = gridLib.stopContainer(promise).thenReturn(promise)
           }
-          return gridLib.removeContainer(promise);
+          return gridLib.removeContainer(promise)
         })
       }))
     }))
-  });
+  })
 }
